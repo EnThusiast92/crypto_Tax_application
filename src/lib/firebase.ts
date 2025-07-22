517350939aedad73f32599fe8d2f8d7ccd3f12d6
PakiEnthusiast <sandybridge9292@gmail.com>
2025-07-22 09:56:09 +0000

    wait. i went inside the firebase web app and found when i goto storage i

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBJSapORSs7ULz9rsExA7s8xUIzjUO_iXw",
   authDomain: "cryptotaxapp-21586.firebaseapp.com",
   projectId: "cryptotaxapp-21586",
-  storageBucket: "cryptotaxapp-21586.firebasestorage.app",
+  // storageBucket: "cryptotaxapp-21586.firebasestorage.app", // REMOVED - Requires billing plan
   messagingSenderId: "296139586728",
   appId: "1:296139586728:web:288ab5819d2aae474b169d",
   measurementId: "G-LZ5F7CGQGX"
